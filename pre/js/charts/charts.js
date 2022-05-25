5a12707328f5745ba3001c2fc5fa2dd72986d66c
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-05-25 16:21:42 +0200

    Cambio de repo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -16,7 +16,7 @@ let tooltip = d3.select('#tooltip');
 
 export function initChart() {
     //Desarrollo del gráfico
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_economia_3_7/main/data/riesgo_pobreza_edad_sexo_v2.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/EnvejecimientoEnRed/informe_perfil_mayores_2022_economia_3_7/main/data/riesgo_pobreza_edad_sexo_v2.csv', function(error,data) {
         if (error) throw error;
 
         ///// Desarrollo de los tres gráficos
